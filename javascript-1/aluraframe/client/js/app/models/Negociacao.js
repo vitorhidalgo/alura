1285d54b9e900da06da51d5feca8cc49f1edd4a3
Vitor Hidalgo <vitor.hidalgo@gmail.com>
2018-01-21 19:15:19 -0200

    02-04 - Encapsulamento

--- javascript-1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-1/aluraframe/client/js/app/models/Negociacao.js
@@ -1,9 +1,29 @@
 class Negociacao
 {
-	constructor()
+	constructor( data, quantidade, valor )
 	{
-		this.data       = new Date();
-		this.quantidade = 1;
-		this.valor      = 0.0;
+		this._data       = data;
+		this._quantidade = quantidade;
+		this._valor      = valor;
+	}
+
+	getVolume()
+	{
+		return this._quantidade * this._valor;
+	}
+
+	getData()
+	{
+		return this._data;
+	}
+
+	getQuantidade()
+	{
+		return this._quantidade;
+	}
+
+	getValor()
+	{
+		return this._valor;
 	}
 }
