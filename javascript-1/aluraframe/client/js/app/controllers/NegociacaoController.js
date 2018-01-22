b4c0cb54fa1efed7eb2b7050829011de7e067d24
Vitor Hidalgo <vitor.hidalgo@gmail.com>
2018-01-21 23:20:06 -0200

    aula 3

--- javascript-1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,17 +4,27 @@ class NegociacaoController
 	{
 		let $ = document.querySelector.bind(document);
 
-		this.inputData       = $('#data');
-		this.inputQuantidade = $('#quantidade');
-		this.inputValor      = $('#valor');
+		this._inputData       = $('#data');
+		this._inputQuantidade = $('#quantidade');
+		this._inputValor      = $('#valor');
 	};
 	
 	adiciona(event)
 	{
 		event.preventDefault();
 
-		console.log(this.inputData.value);
-		console.log(this.inputQuantidade.value);
-		console.log(this.inputValor.value);
+		let data = new Date
+		(
+			...this._inputData.value.split('-').map((item, index) => item - index % 2 )
+		);
+
+		let negociacao = new Negociacao
+		(
+			data,
+			this._inputQuantidade.value,
+			this._inputValor.value
+		);
+
+		console.log(negociacao);
 	}
 }
