6f7fb07eff06ad0961de64d60364cf3eb077dcb7
Vitor Hidalgo <vitor.hidalgo@gmail.com>
2018-03-01 18:48:02 -0300

    javascript 2

---
+++ javascript-2/exercicios/aula-6-8.js
@@ -0,0 +1,17 @@
+class Conta
+{
+    constructor(saldo)
+    {
+        this._saldo = saldo;
+    }
+
+    get saldo()
+    {
+        return this._saldo;
+    }
+
+    atualiza(taxa) 
+    {
+        throw new Error('Você deve sobrescrever o método');   
+    }
+}
